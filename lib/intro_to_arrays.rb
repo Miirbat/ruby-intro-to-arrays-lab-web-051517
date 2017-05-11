acb21dcac51039b7609f3a4f23c13db81506fea3
Mirna Plakalovic <mirna.plakalovic@gmail.com>
2017-05-11 21:19:51 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["lasers", "lenses"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def remove_element_from_start_of_array(array)
+  array.shift
+end
+
+def retrieve_element_from_index(array, index_number)
+  array[index_number]
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.count
+end
